8bfe5d84d90b96af4d4d39a0e1dc412ec85e957e
Anshu Madhikarmi <madhikarmianshu@gmail.com>
2023-10-09 06:31:49 +0545

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -63,7 +63,7 @@ function Quiz() {
 
     // Load environment variables for email service
     const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const templateId = process.env.REACT_APP_EMAILJS_QUESTIONS_TEMPLATE_ID;
     const userId = process.env.REACT_APP_EMAILJS_USER_ID;
 
     // Send the form using EmailJS
